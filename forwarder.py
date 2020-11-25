7152ae69f7dc5e73113660b1d404abcfc532ca27
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-25 12:44:51 +0530

    update

---
+++ forwarder.py
@@ -0,0 +1,55 @@
+''' A script to send all messages from one chat to another '''
+
+import asyncio
+import logging
+
+from telethon.tl.patched import MessageService
+from telethon import TelegramClient
+from settings import API_ID, API_HASH, forwards, get_forward, update_offset
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+async def forward_job():
+    ''' the function that does the job 😂 '''
+
+    async with TelegramClient('forwarder', API_ID, API_HASH) as client:
+        for forward in forwards:
+            from_chat, to_chat, offset = get_forward(forward)
+            _from, _to = None, None
+
+            async for dialog in client.iter_dialogs():
+                if dialog.name == to_chat:
+                    _to = dialog
+                elif dialog.name == from_chat:
+                    _from = dialog
+                if (_from and _to):
+                    break
+
+            if not (_from and _to):
+                logging.warning(
+                    'Make sure to have correct `from` and `to` for %s in config.ini', forward)
+                continue
+            logging.info(
+                'Succesfully got the chat of `from` and `to` for %s', forward)
+            if not offset:
+                offset = 0
+
+            last_id = 0
+            async for message in client.iter_messages(from_chat, reverse=True, offset_id=offset):
+                if isinstance(message, MessageService):
+                    continue
+                try:
+                    await client.send_message(to_chat, message)
+                    last_id = str(message.id)
+                    logging.info('forwarding message with id = %s', last_id)
+                except Exception as err:
+                    logging.exception(err)
+
+            update_offset(forward, last_id)
+            logging.info('Completed working with %s', forward)
+
+
+asyncio.run(forward_job())
---
+++ settings.py
@@ -0,0 +1,46 @@
+from configparser import ConfigParser
+from dotenv import load_dotenv
+import os
+import logging
+
+load_dotenv()
+
+API_ID = os.getenv('api_id')
+API_HASH = os.getenv('api_hash')
+
+assert API_ID and API_HASH
+
+configur = ConfigParser()
+configur.read('config.ini')
+
+forwards = configur.sections()
+
+assert forwards
+
+
+def get_forward(forward: str) -> tuple:
+    try:
+        from_chat = configur.get(forward, 'from')
+        to_chat = configur.get(forward, 'to')
+        offset = configur.getint(forward, 'offset')
+        return from_chat, to_chat, offset
+    except Exception as err:
+        logging.exception(
+            'The content of %s does not follow format. See the README.md file for more details. \n\n %s', forward, str(err))
+        quit()
+
+
+def update_offset(forward: str, new_offset: str) -> None:
+    try:
+        configur.set(forward, 'offset', new_offset)
+        with open('config.ini', 'w') as cfg:
+            configur.write(cfg)
+    except Exception as err:
+        logging.exception(
+            'Problem occured while updating offset of %s \n\n %s', forward, str(err))
+
+
+if __name__ == "__main__":
+    # testing
+    for forward in forwards:
+        print(forward, get_forward(forward))
