77a9747d6e2da5589433ca849b673e13168c44f6
aahnik <daw@aahnik.dev>
2021-04-03 21:29:35 +0530

    you are history

--- tgcf/past.py
+++ tgcf/past.py
@@ -5,30 +5,29 @@ import logging
 from telethon.tl.patched import MessageService
 from telethon.errors.rpcerrorlist import FloodWaitError
 
-from tgcf.config import Config
 
+async def forward_job():
+    from tgcf.config import CONFIG, API_ID, API_HASH, SESSION
+    async with TelegramClient(SESSION, API_ID, API_HASH) as client:
 
-async def forward_job(client: TelegramClient, config: Config):
-    await client.start()
-    for forward in config.forwards:
-        last_id = 0
-        print(forward.source)
-        print(forward.dest)
-        async for message in client.iter_messages(forward.source,
-                                                  reverse=True,
-                                                  offset_id=forward.offset):
-            if isinstance(message, MessageService):
-                continue
-            try:
-                for destination in forward.dest:
-                    await client.send_message(destination, message)
-                last_id = str(message.id)
-                logging.info('forwarding message with id = %s', last_id)
-                forward.offset = last_id
-            except FloodWaitError as fwe:
-                print(f'Sleeping for {fwe}')
-                await asyncio.sleep(delay=fwe.seconds)
-            except Exception as err:
-                logging.exception(err)
-                break
-    await client.disconnect()
+        for forward in CONFIG.forwards:
+            last_id = 0
+            print(forward.source)
+            print(forward.dest)
+            async for message in client.iter_messages(forward.source,
+                                                      reverse=True,
+                                                      offset_id=forward.offset):
+                if isinstance(message, MessageService):
+                    continue
+                try:
+                    for destination in forward.dest:
+                        await client.send_message(destination, message)
+                    last_id = str(message.id)
+                    logging.info('forwarding message with id = %s', last_id)
+                    forward.offset = last_id
+                except FloodWaitError as fwe:
+                    print(f'Sleeping for {fwe}')
+                    await asyncio.sleep(delay=fwe.seconds)
+                except Exception as err:
+                    logging.exception(err)
+                    break
