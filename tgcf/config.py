e2c60d7d0547c6577d667cbb33ed613718a55e24
aahnik <daw@aahnik.dev>
2021-04-03 21:29:05 +0530

    the config.py

--- tgcf/config.py
+++ tgcf/config.py
@@ -1,16 +1,24 @@
 # a custom config parser
+
+
 import yaml
 from typing import List
 from pydantic import BaseModel
-from tgcf.const import CONFIG_FILE
+import os
+from telethon.sessions import StringSession
+
+CONFIG_FILE = 'tgcf.config.yml'
+
 
 class Forward(BaseModel):
     source: int
     dest: List[int]
     offset: int
 
+
 class Config(BaseModel):
     forwards: List[Forward]
+
 
 def read_config():
     with open(CONFIG_FILE) as file:
@@ -23,7 +31,30 @@ def read_config():
         else:
             return config
 
-def update_config(config:Config):
-    with open(CONFIG_FILE,'w') as file:
-        yaml.dump(config.dict(),file)
 
+def update_config(config: Config):
+    with open(CONFIG_FILE, 'w') as file:
+        yaml.dump(config.dict(), file)
+
+
+def env_var(name: str, optional=False):
+    var = os.getenv(name)
+
+    while not var:
+        if optional:
+            break
+        var = input(f'Enter {name}: ')
+    return var
+
+
+API_ID = env_var('API_ID')
+API_HASH = env_var('API_HASH')
+USERNAME = env_var('USERNAME', optional=True)
+SESSION_STRING = env_var('SESSION_STRING', optional=True)
+
+if SESSION_STRING:
+    SESSION = StringSession(SESSION_STRING)
+else:
+    SESSION = 'tgcf'
+
+CONFIG = read_config()
