f17f7c68ad9764996edc55890e15b907cd822951
aahnik <daw@aahnik.dev>
2021-04-03 21:29:27 +0530

    you are main

--- tgcf/main.py
+++ tgcf/main.py
@@ -2,18 +2,16 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 import asyncio
 from tgcf.past import forward_job
-from tgcf.config import read_config
-
-config = read_config()
 
 
 
-def start_past(client):
-    asyncio.run(forward_job(client, config))
 
 
-def start_live(client):
-    pass
+def start_past():
+    asyncio.run(forward_job())
 
-def start_tgcf(mode):
-    pass
+
+
+def start_live():
+    print('live')
+
