354e0738a425a584441e53c401b1850265e03689
Aahnik Daw <daw@aahnik.dev>
2021-04-23 09:20:05 +0530

    also install micro : a text editor

--- scripts/install_on_termux.sh
+++ scripts/install_on_termux.sh
@@ -1,6 +1,6 @@
 termux-info
 pkg upgrade
-pkg install libjpeg-turbo python -y
+pkg install libjpeg-turbo python micro -y
 pip install --upgrade pip wheel setuptools
 pip install --upgrade tgcf
 tgcf --version
