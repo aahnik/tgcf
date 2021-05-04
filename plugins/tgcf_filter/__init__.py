7135388830750da4239e487923fdc57107a044e9
aahnik <daw@aahnik.dev>
2021-05-04 22:10:47 +0530

    add filter by sender user
    only int id supported as of now

--- plugins/tgcf_filter/__init__.py
+++ plugins/tgcf_filter/__init__.py
@@ -5,12 +5,14 @@ from pydantic import BaseModel
 
 
 class FilterList(BaseModel):
-    blacklist: Optional[List[str]] = []
-    whitelist: Optional[List[str]] = []
+    blacklist: Optional[List[str or int]] = []
+    whitelist: Optional[List[str or int]] = []
 
 
 class Filters(BaseModel):
-    text: Optional[FilterList]
+    users: Optional[FilterList] = FilterList()
+    files: Optional[FilterList] = FilterList()
+    text: Optional[FilterList] = FilterList()
 
 
 class TgcfFilter:
@@ -22,14 +24,17 @@ class TgcfFilter:
         logging.info(self.filters)
 
     def modify(self, message):
-        msg_text = message.text
-        if not msg_text and self.filters.text.whitelist == []:
-            return message
 
-        if self.text_safe(msg_text, self.filters.text):
-            return message
+        if self.users_safe(message):
+            if self.files_safe(message):
+                if self.text_safe(message):
+                    return message
 
-    def text_safe(self, text, flist: FilterList):
+    def text_safe(self, message):
+        flist = self.filters.text
+        text = message.text
+        if not text and flist.whitelist == []:
+            return True
         for forbidden in flist.blacklist:
             if forbidden in text:
                 return False
@@ -38,3 +43,18 @@ class TgcfFilter:
         for allowed in flist.whitelist:
             if allowed in text:
                 return True
+
+    def users_safe(self, message):
+        flist = self.filters.users
+        sender = str(message.sender_id)
+        logging.info(f"M message from sender id {sender}")
+        if sender in flist.blacklist:
+            return False
+        if not flist.whitelist:
+            return True
+        if sender in flist.whitelist:
+            return True
+
+    def files_safe(self, message):
+        flist = self.filters.files
+        return True
