8145a172d409b2d22a06c98d3806ee867b372c12
aahnik <daw@aahnik.dev>
2021-05-24 16:57:24 +0530

    attempt to solve #167, proper yaml dump
    -> sort_keys = False
    -> default_flow_style = None

--- tgcf/config.py
+++ tgcf/config.py
@@ -116,7 +116,7 @@ def update_config_file(config: Config):
     """Write changes in config back to file."""
     if CONFIG_TYPE == 1 or CONFIG_TYPE == 0:
         with open(CONFIG_FILE_NAME, "w") as file:
-            yaml.dump(config.dict(), file)
+            yaml.dump(config.dict(), file,sort_keys=False,default_flow_style=None)
     elif CONFIG_TYPE == 2:
         logging.warning("Could not update config! As env var is used")
 
