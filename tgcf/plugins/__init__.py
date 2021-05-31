2b94d91ff2ed89c9eaf56e1c2db79e98ab4b18ce
aahnik <daw@aahnik.dev>
2021-05-31 12:03:06 +0530

    move plugins.py to plugins/__init__.py

--- tgcf/plugins/__init__.py
+++ tgcf/plugins/__init__.py
@@ -1,4 +1,8 @@
-"""Load and apply plugins."""
+"""Subpackage of tgcf: plugins.
+
+Contains all the first-party tgcf plugins.
+"""
+
 
 import logging
 from importlib import import_module
@@ -13,16 +17,33 @@ def load_plugins():
     """Load the plugins specified in config."""
     _plugins = {}
     for plugin_id, plugin_data in PLUGINS.items():
-        plugin_module_name = f"tgcf_{plugin_id}"
+
         plugin_class_name = f"Tgcf{plugin_id.title()}"
+
         try:
-            plugin_module = import_module(plugin_module_name)
+            plugin_module = import_module("tgcf.plugins." + plugin_id)
+        except ModuleNotFoundError:
+            logging.error(
+                f"{plugin_id} is not a first party plugin. Trying to load from availaible third party plugins."
+            )
+            try:
+                plugin_module_name = f"tgcf_{plugin_id}"
+                plugin_module = import_module(plugin_module_name)
+            except ModuleNotFoundError:
+                logging.error(
+                    f"Module {plugin_module_name} not found. Failed to load plugin {plugin_id}"
+                )
+                continue
+            else:
+                logging.info(
+                    f"Plugin {plugin_id} is successfully loaded from third party pluguins"
+                )
+        else:
+            logging.info(f"First party plugin {plugin_id} loaded!")
+        try:
             plugin_class = getattr(plugin_module, plugin_class_name)
             plugin = plugin_class(plugin_data)
             assert plugin.id_ == plugin_id
-
-        except ModuleNotFoundError:
-            logging.error(f"Could not find plugin for {plugin_id}")
         except AttributeError:
             logging.error(f"Found plugin {plugin_id}, but failed to load.")
         else:
