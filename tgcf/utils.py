be94aa35248af5e2f17e9ad3b2e38d0641e15c14
aahnik <daw@aahnik.dev>
2021-06-01 14:26:12 +0530

    update send_message in utils and add two new funcs
    -> send_message now deals with TgcfMessage object
    
    -> client argument is no longer needed,
    as the telethon's Message object has it within
    
    -> add two new funcs stamp and safe_name

--- tgcf/utils.py
+++ tgcf/utils.py
@@ -1,11 +1,55 @@
 """Utility functions to smoothen your life."""
 
+import logging
+import os
+import re
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from telethon.client import TelegramClient
+from telethon.hints import EntityLike
+
 from tgcf.config import CONFIG
 
+if TYPE_CHECKING:
+    from tgcf.plugins import TgcfMessage
 
-async def send_message(client, *args):
+
+async def send_message(recipient: EntityLike, tm: "TgcfMessage"):
     """Forward or send a copy, depending on config."""
-    # show forwarded from
+    client: TelegramClient = tm.message.client
     if CONFIG.show_forwarded_from:
-        return await client.forward_messages(*args)
-    return await client.send_message(*args)
+        return await client.forward_messages(recipient, tm.message)
+    if tm.new_file:
+        message = await client.send_file(recipient, tm.new_file, caption=tm.text)
+        return message
+    tm.message.text = tm.text
+    return await client.send_message(recipient, tm.message)
+
+
+def cleanup(*files):
+    """Delete the file names passed as args."""
+    for file in files:
+        try:
+            os.remove(file)
+        except FileNotFoundError:
+            logging.info(f"File {file} does not exist, so cant delete it.")
+
+
+def stamp(file: str, user: str):
+    """Stamp the filename with the datetime, and user info."""
+    now = str(datetime.now())
+    outf = safe_name(f"{user} {now} {file}")
+    try:
+        os.rename(file, outf)
+        return outf
+    except Exception as err:
+        logging.warning(f"Stamping file name failed for {file} to {outf}. \n {err}")
+
+
+def safe_name(string: str):
+    """Return safe file name.
+
+    Certain characters in the file name can cause potential problems in rare scenarios.
+    """
+    return re.sub(pattern=r"[-!@#$%^&*()\s]", repl="_", string=string)
