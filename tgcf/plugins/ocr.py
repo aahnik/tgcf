902712e2f3078bee0881af1559edbb07e39bd32b
aahnik <daw@aahnik.dev>
2021-06-01 15:28:57 +0530

    add plugin ocr

--- tgcf/plugins/ocr.py
+++ tgcf/plugins/ocr.py
@@ -1,8 +1,22 @@
-class TgcfOcr:
+import pytesseract
+from PIL import Image
+
+from tgcf.plugins import TgcfMessage, TgcfPlugin
+from tgcf.utils import cleanup
+
+
+class TgcfOcr(TgcfPlugin):
     id_ = "ocr"
 
     def __init__(self, data) -> None:
         pass
 
-    def modify(self, message):
-        pass
+    async def modify(self, tm: TgcfMessage):
+
+        if not tm.file_type in ["photo"]:
+            return tm
+
+        file = await tm.get_file()
+        tm.text = pytesseract.image_to_string(Image.open(file))
+        cleanup(file)
+        return tm
