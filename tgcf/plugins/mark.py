f62e7d2f44790768454cf824b7060f07e174f087
aahnik <daw@aahnik.dev>
2021-06-01 20:38:16 +0530

    allow direct image url for watermark plugin

--- tgcf/plugins/mark.py
+++ tgcf/plugins/mark.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 from typing import Any, Dict
 
+import requests
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from watermark import File, Position, Watermark, apply_watermark
 
@@ -13,6 +16,26 @@ class MarkConfig(BaseModel):
     frame_rate: int = 15
 
 
+def download_image(url: str, filename: str = "image.png") -> bool:
+    if filename in os.listdir():
+        print("Image exists")
+        return True
+    try:
+        print("Downloading image ... ")
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            print("Got file response")
+            with open(filename, "wb") as file:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, file)
+    except Exception as err:
+        print(err)
+        return False
+    else:
+        print("File created image")
+        return True
+
+
 class TgcfMark(TgcfPlugin):
     id_ = "mark"
 
@@ -24,7 +47,11 @@ class TgcfMark(TgcfPlugin):
             return tm
         downloaded_file = await tm.get_file()
         base = File(downloaded_file)
-        overlay = File(self.data.image)
+        if self.data.image.startswith("https://"):
+            download_image(self.data.image)
+            overlay = File("image.png")
+        else:
+            overlay = File(self.data.image)
         wtm = Watermark(overlay, self.data.position)
         tm.new_file = apply_watermark(base, wtm, frame_rate=self.data.frame_rate)
         cleanup(downloaded_file)
