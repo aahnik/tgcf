d33762104c904530289018fe05b626b2f7f7b823
aahnik <daw@aahnik.dev>
2021-06-01 14:22:39 +0530

    massive update for plugin system
    -> create TgcfPlugin class, all plugins must inherit from it
    
    -> create a TgcfMessage class
    the message which will be thrown to plugins will of this type
    
    TgcfMessage is a wrapper for Telethon's Message
    
    -> plugin loading and application has enhanced error handling

--- tgcf/plugins/__init__.py
+++ tgcf/plugins/__init__.py
@@ -4,20 +4,78 @@ Contains all the first-party tgcf plugins.
 """
 
 
-import logging
-from importlib import import_module
-from typing import List
 import inspect
+import logging
+from enum import Enum
+from importlib import import_module
+from typing import Any, Dict
+
+from telethon.tl.custom.message import Message
+
 from tgcf.config import CONFIG
+from tgcf.utils import cleanup, stamp
 
 PLUGINS = CONFIG.plugins
 
 
-def load_plugins():
+class FileType(str, Enum):
+    AUDIO = "audio"
+    GIF = "gif"
+    VIDEO = "video"
+    VIDEO_NOTE = "video_note"
+    STICKER = "sticker"
+    CONTACT = "contact"
+    PHOTO = "photo"
+    DOCUMENT = "document"
+    NOFILE = "nofile"
+
+
+class TgcfMessage:
+    def __init__(self, message: Message):
+        self.message = message
+        self.text = self.message.text
+        self.raw_text = self.message.raw_text
+        self.sender_id = self.message.sender_id
+        self.file_type = self.guess_file_type()
+        self.new_file = None
+        self.cleanup = False
+
+    async def get_file(self):
+        """Downloads the file in the message and returns the path where its saved."""
+        self.file = stamp(await self.message.download_media(""), self.sender_id)
+        return self.file
+
+    def guess_file_type(self):
+        for i in FileType:
+            if i == FileType.NOFILE:
+                return i
+            obj = getattr(self.message, i.value)
+            if obj:
+                return i
+
+    def clear(self):
+        if self.new_file and self.cleanup:
+            cleanup(self.new_file)
+            self.new_file = None
+
+
+class TgcfPlugin:
+    id_ = "plugin"
+
+    def __init__(self, data: Dict[Any, Any]):
+        self.data = data
+
+    def modify(self, tm: TgcfMessage):
+        """Modify the message here."""
+        return tm
+
+
+def load_plugins() -> Dict[str, TgcfPlugin]:
     """Load the plugins specified in config."""
     _plugins = {}
     for plugin_id, plugin_data in PLUGINS.items():
-
+        if not plugin_data:
+            plugin_data = {}
         plugin_class_name = f"Tgcf{plugin_id.title()}"
 
         try:
@@ -42,27 +100,41 @@ def load_plugins():
             logging.info(f"First party plugin {plugin_id} loaded!")
         try:
             plugin_class = getattr(plugin_module, plugin_class_name)
-            plugin = plugin_class(plugin_data)
-            assert plugin.id_ == plugin_id
+            if not issubclass(plugin_class, TgcfPlugin):
+                logging.error(
+                    f"Plugin class {plugin_class_name} does not inherit TgcfPlugin"
+                )
+                continue
+            plugin: TgcfPlugin = plugin_class(plugin_data)
+            if not plugin.id_ == plugin_id:
+                logging.error(f"Plugin id for {plugin_id} does not match expected id.")
+                continue
         except AttributeError:
-            logging.error(f"Found plugin {plugin_id}, but failed to load.")
+            logging.error(f"Found plugin {plugin_id}, but plguin class not found.")
         else:
-            print(f"Loaded plugin {plugin_id}")
+            logging.info(f"Loaded plugin {plugin_id}")
             _plugins.update({plugin.id_: plugin})
     return _plugins
 
 
-async def apply_plugins(message) -> List:
+async def apply_plugins(message: Message) -> TgcfMessage:
     """Apply all loaded plugins to a message."""
+    tm = TgcfMessage(message)
+
     for _id, plugin in plugins.items():
-        if inspect.iscoroutinefunction(plugin.modify):
-            message = await plugin.modify(message)
+        try:
+            if inspect.iscoroutinefunction(plugin.modify):
+                ntm = await plugin.modify(tm)
+            else:
+                ntm = plugin.modify(tm)
+        except Exception as err:
+            logging.error(f"Failed to apply plugin {_id}. \n {err} ")
         else:
-            message = plugin.modify(message)
-        logging.info(f"Applied plugin {_id}")
-        if not message:
-            return None
-    return message
+            logging.info(f"Applied plugin {_id}")
+            if not ntm:
+                tm.clear()
+                return None
+    return tm
 
 
 plugins = load_plugins()
