ddccebb1c6664f3be1a8d9ab28f48f825354a3a3
Aahnik Daw <daw@aahnik.dev>
2022-11-11 22:23:34 +0530

    update Advanced page in webui

--- tgcf/web_ui/pages/6_🔬_Advanced.py
+++ tgcf/web_ui/pages/6_🔬_Advanced.py
@@ -3,6 +3,7 @@ import json
 import streamlit as st
 
 from tgcf.config import CONFIG_FILE_NAME, read_config
+from tgcf.utils import platform_info
 from tgcf.web_ui.password import check_password
 from tgcf.web_ui.utils import hide_st
 
@@ -15,14 +16,19 @@ st.set_page_config(
 hide_st(st)
 
 if check_password(st):
+
     st.warning("This page is for developers and advanced users.")
     if st.checkbox("I agree"):
-        with open(CONFIG_FILE_NAME, "r") as file:
-            data = json.loads(file.read())
-            dumped = json.dumps(data, indent=3)
 
-        with st.expander("View config json"):
+        with st.expander("Version & Platform"):
+            st.code(platform_info())
+
+        with st.expander("Configuration"):
+            with open(CONFIG_FILE_NAME, "r") as file:
+                data = json.loads(file.read())
+                dumped = json.dumps(data, indent=3)
+            st.download_button(
+                f"Download config json", data=dumped, file_name=CONFIG_FILE_NAME
+            )
+
             st.json(data)
-        st.download_button(
-            f"Download config json", data=dumped, file_name=CONFIG_FILE_NAME
-        )
